b566aba9f7a5ad3f45528086027c6ce0073025ae
Gustavo Bezerras <gustavobs16@gmail.com>
2021-06-08 14:53:36 -0400

    home layout

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,30 +1,39 @@
 <template>
-    <div>
-        <h1>Home</h1>
-        <Button
-          label="Clientes"
-          icon="pi pi-times"
-          @click="$router.push('listaClientes')"
-          class="p-button-text"
-        />
-        <Button
-          label="Linhas"
-          icon="pi pi-times"
-          @click="$router.push('listaLinhas')"
-          class="p-button-text"
-        />
-        <Button
-          label="Estruturas"
-          icon="pi pi-times"
-          @click="$router.push('listaEstruturas')"
-          class="p-button-text"
-        />
-        <Button
-          label="Dispositivos"
-          icon="pi pi-times"
-          @click="$router.push('listaDispositivos')"
-          class="p-button-text"
-        />
+    <div style="margin: 0 auto; width: 50%">
+        
+        <div class="name">
+            <h1 >Gerenciador de Dispositivos</h1>
+        </div>
+        
+        <div class="foto">
+            <img src="../assets/Nastek.png" alt="Italian Trulli">
+        </div>
+        <div class="buttons">
+            <Button
+              label="Clientes"
+              icon="pi pi-user"
+              @click="$router.push('listaClientes')"
+              class="p-button-text"
+            />
+            <Button
+              label="Linhas"
+              icon="pi pi-share-alt"
+              @click="$router.push('listaLinhas')"
+              class="p-button-text"
+            />
+            <Button
+              label="Estruturas"
+              icon="pi pi-ticket"
+              @click="$router.push('listaEstruturas')"
+              class="p-button-text"
+            />
+            <Button
+              label="Dispositivos"
+              icon="pi pi-sitemap"
+              @click="$router.push('listaDispositivos')"
+              class="p-button-text"
+            />
+        </div>
     </div>    
 </template>
 
@@ -36,5 +45,21 @@ export default {
 </script>
 
 <style>
+.name {
+    display: block;
+    margin-left: 100px;
+    width: 100%;
+    color: #007ad9;
+}
 
+.foto {
+    width: 50%;
+    margin: auto;
+    display: block;
+}
+
+.buttons {
+    margin-left: 50px;
+    display: block;
+}
 </style>
